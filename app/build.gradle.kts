4288d3ef9baf129bd0f65402ef655994e4f7afa8
devvcraze <f20230939@goa.bits-pilani.ac.in>
2024-07-20 10:36:44 +0530

    Revert "error done"
    This reverts commit fcb943e82b2e12bacf02711b900479f95b7fd8e8.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,9 +60,6 @@ dependencies {
     implementation(libs.dialogplus)
     implementation(libs.firebase.bom)
     implementation(libs.firebase.messaging)
-    implementation(libs.coil)
-    implementation(libs.com.google.android.material.material)
-    implementation(libs.design)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
--- app/src/main/java/com/example/abhigyaan/aboutActivity.java
+++ app/src/main/java/com/example/abhigyaan/aboutActivity.java
@@ -1,9 +1,6 @@
 package com.example.abhigyaan;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,31 +15,10 @@ public class aboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_about);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void openInstagram(View view) {
-        openUrl("https://www.instagram.com/abhigyaan_bitsgoa?igsh=cmxlbzk2ZDd6YjQ5");
-    }
-
-    public void openLinkedIn(View view) {
-        openUrl("https://www.linkedin.com/company/abhigyaan/mycompany/");
-    }
-
-    public void openFacebook(View view) {
-        openUrl("https://www.facebook.com/abhigyaanbpgc/");
-    }
-
-    public void openWebsite(View view) {
-        openUrl("https://www.yourwebsite.com");
-    }
-
-    private void openUrl(String url) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-        startActivity(intent);
     }
 }
