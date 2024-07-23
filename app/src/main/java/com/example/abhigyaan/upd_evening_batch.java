de310df47f54e57d5fa32674a235a51d8892bc90
devvcraze <f20230939@goa.bits-pilani.ac.in>
2024-07-23 15:22:32 +0530

    Revert "error"
    This reverts commit 30ecedfea28ab509ecc1c3ecbe80d15a19eba2de.

--- app/src/main/java/com/example/abhigyaan/upd_evening_batch.java
+++ app/src/main/java/com/example/abhigyaan/upd_evening_batch.java
@@ -6,28 +6,18 @@ import android.view.MenuItem;
 import android.widget.ProgressBar;
 import android.widget.SearchView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class upd_evening_batch extends AppCompatActivity {
     RecyclerView recyclerView;
     MainAdapter_evening mainAdapterDisha;
     ProgressBar progressBar;
-    private DatabaseReference userRef;
-    private FirebaseAuth mAuth;
-    private boolean updateMade = false; // Flag to track if an update is made
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,45 +39,12 @@ public class upd_evening_batch extends AppCompatActivity {
                 super.onDataChanged();
                 // Hide the progress bar when data is loaded
                 progressBar.setVisibility(android.view.View.GONE);
-                if (updateMade) {
-                    incrementClassesTaken();
-                }
             }
         };
         recyclerView.setAdapter(mainAdapterDisha);
 
         // Show the progress bar when data is being loaded
         progressBar.setVisibility(android.view.View.VISIBLE);
-
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
-        FirebaseUser user = mAuth.getCurrentUser();
-        if (user != null) {
-            String userId = user.getUid();
-            userRef = FirebaseDatabase.getInstance().getReference().child("users").child(userId);
-        }
-    }
-
-    private void incrementClassesTaken() {
-        if (userRef != null) {
-            userRef.child("classesTaken").addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    if (snapshot.exists()) {
-                        int classesTaken = snapshot.getValue(Integer.class);
-                        userRef.child("classesTaken").setValue(classesTaken + 1);
-                    } else {
-                        userRef.child("classesTaken").setValue(1);
-                    }
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-                    // Handle error if needed
-                }
-            });
-            updateMade = false; // Reset the flag
-        }
     }
 
     @Override
@@ -134,9 +91,6 @@ public class upd_evening_batch extends AppCompatActivity {
                 super.onDataChanged();
                 // Hide the progress bar when data is loaded
                 progressBar.setVisibility(android.view.View.GONE);
-                if (updateMade) {
-                    incrementClassesTaken();
-                }
             }
         };
         mainAdapterDisha.startListening();
@@ -144,10 +98,5 @@ public class upd_evening_batch extends AppCompatActivity {
 
         // Show the progress bar when data is being loaded
         progressBar.setVisibility(android.view.View.VISIBLE);
-    }
-
-    // Call this method whenever an update is actually made
-    public void setUpdateMade(boolean updateMade) {
-        this.updateMade = updateMade;
     }
 }
