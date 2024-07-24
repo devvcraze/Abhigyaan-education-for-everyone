1b39e6ba4a77b43e5d41cf3e758a3780d43d955d
devvcraze <f20230939@goa.bits-pilani.ac.in>
2024-07-24 09:14:47 +0530

    gallery added to all the verticals

--- app/src/main/java/com/example/abhigyaan/gal_disha_batch.java
+++ app/src/main/java/com/example/abhigyaan/gal_disha_batch.java
@@ -7,6 +7,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class gal_disha_batch extends AppCompatActivity {
 
@@ -20,5 +25,18 @@ public class gal_disha_batch extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        RecyclerView recyclerView = findViewById(R.id.rv);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        List<ImageModel> imageList = new ArrayList<>();
+        imageList.add(new ImageModel("https://drive.google.com/uc?export=view&id=1_xe0CvkWmoE2iOx3PtjW-XaErlUZSlXv"));
+        imageList.add(new ImageModel("https://drive.google.com/uc?export=view&id=1mRRrXgExJG52_xarIQbRjyZTg0wdEJGZ"));
+        imageList.add(new ImageModel("image1")); // Testing with another source
+
+        // Add more image URLs
+
+        ImageAdapter imageAdapter = new ImageAdapter(imageList);
+        recyclerView.setAdapter(imageAdapter);
     }
 }
