05288fca715f3403d5bbfd9eb53913214ea638d1
devvcraze <f20230939@goa.bits-pilani.ac.in>
2024-07-25 09:09:35 +0530

    Revert "error"
    This reverts commit 3fee040c2dd06e5a4f478dba24dcd62f92c0b8cf.

--- app/src/main/java/com/example/abhigyaan/MainAdapter_evening.java
+++ app/src/main/java/com/example/abhigyaan/MainAdapter_evening.java
@@ -27,7 +27,7 @@ import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public abstract class MainAdapter_evening extends FirebaseRecyclerAdapter<MainModel_disha, MainAdapter_evening.myViewHolder> {
+public class MainAdapter_evening extends FirebaseRecyclerAdapter<MainModel_disha, MainAdapter_evening.myViewHolder> {
     private boolean toastShown = false;
 
     public MainAdapter_evening(@NonNull FirebaseRecyclerOptions<MainModel_disha> options) {
@@ -121,10 +121,6 @@ public abstract class MainAdapter_evening extends FirebaseRecyclerAdapter<MainMo
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.main_item, parent, false);
         return new myViewHolder(view);
     }
-
-    public abstract void onUpdateAction();
-
-    public abstract void onUpdateAction(String key);
 
     static class myViewHolder extends RecyclerView.ViewHolder {
 
